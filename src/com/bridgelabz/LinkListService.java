0b1ac42f9a7252f65695e3a4f189c78a9de537ac
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-09-13 21:33:15 +0530

    Added:Method to Append NOdes

--- src/com/bridgelabz/LinkListService.java
+++ src/com/bridgelabz/LinkListService.java
@@ -3,19 +3,45 @@ package com.bridgelabz;
 public class LinkListService {
 	LinkList head;
 
-	public void add(int data) {
+	public void add(int data) 
+	{
 		LinkList newNode = new LinkList();
 		newNode.data = data;
 		newNode.next = head;
 		this.head = newNode;
 	}
 
-	public void print() {
+	public void append(int data)
+	{
+		LinkList newNode = new LinkList();
+		newNode.data = data;
+		newNode.next = null;
+		if (head == null) 
+		{			
+			this.head = newNode;
+		}
+		else
+		{
+			LinkList currentNode = head;
+			while (currentNode.next != null)
+			{
+				currentNode = currentNode.next;
+			}
+			currentNode.next = newNode;
+		}
+	}
+	
+	public void print()
+	{
 		LinkList tempNode = head;
-		while (tempNode.next != null) {
-			System.out.print(tempNode.data + " -> ");
+		System.out.println("The required linklist is");
+		while(tempNode.next != null)
+		{
+			System.out.print(tempNode.data+" -> ");
 			tempNode = tempNode.next;
 		}
 		System.out.print(tempNode.data);
 	}
 }
+
+	
